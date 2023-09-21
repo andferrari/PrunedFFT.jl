d2af17675b14007466234f48005358f971e373d0
André Ferrari <andferrari@gmail.com>
2023-09-21 13:46:34 +0200

    CUDA Extension

--- ext/PrunedFFTExt.jl
+++ ext/PrunedFFTExt.jl
@@ -1,8 +1,9 @@
-module PrunedFFT
+module PrunedFFTExt
 
+using PrunedFFT
 using FFTW
+using CUDA: CuMatrix, CUDA.zeros
 
-export pruned_fft
 
 """
     pruned_fft(x::Matrix{Float64}, n_pad::Int, k_span::Vector{Int64})
@@ -10,7 +11,7 @@ export pruned_fft
     Compute the FFT of a (n, n) matrix x zero padded to (n\\_pad, n\\_pad)
     at frequencies (k, l), k ∈ k\\_span, l ∈ k\\_span.
 """
-function pruned_fft(x::Matrix{T}, n_pad::Int, k_span::Vector{Int}) where {T <: Real}
+function PrunedFFT.pruned_fft(x::CuMatrix{T}, n_pad::Int, k_span::Vector{Int}) where {T <: Real}
 
     n, l = size(x)
     nk = length(k_span)
@@ -19,15 +20,15 @@ function pruned_fft(x::Matrix{T}, n_pad::Int, k_span::Vector{Int}) where {T <: R
     @assert (0 < minimum(k_span)) &  (maximum(k_span) < n_pad)
 
     # FFT wrt dim 2 (vertical)
-    P = plan_fft(zeros(T, n_pad))
-    tmp = Matrix{ComplexF64}(undef, n, nk)
+    P = plan_fft(CUDA.zeros(T, n_pad))
+    tmp = CuMatrix{ComplexF32}(undef, n, nk)
     for i in 1:n
         @inbounds tmp[i, :] = transpose((P*[x[i,:]; zeros(n_pad - n)])[k_span])
     end
 
     # FFT wrt dim 1 (horizontal)
-    P = plan_fft(zeros(ComplexF64, n_pad))
-    p_fft = Matrix{ComplexF64}(undef, nk, nk)
+    P = plan_fft(CUDA.zeros(ComplexF32, n_pad))
+    p_fft = CuMatrix{ComplexF32}(undef, nk, nk)
     for j in 1:nk
         @inbounds p_fft[:, j] = (P*[tmp[:, j]; zeros(n_pad - n)])[k_span]
     end
